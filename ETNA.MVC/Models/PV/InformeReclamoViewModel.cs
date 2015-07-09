c8474896186c56d0bc5e3578a29e22aeaa4d06f2
Victor Ramos <victor.manuel.ramos@outlook.com>
2015-07-09 08:46:12 -0500

    Cambios Edit

--- ETNA.MVC/Models/PV/InformeReclamoViewModel.cs
+++ ETNA.MVC/Models/PV/InformeReclamoViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
 namespace ETNA.MVC.Models.PV
 {
     public class InformeReclamoViewModel
