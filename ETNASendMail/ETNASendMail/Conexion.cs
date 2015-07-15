3cb96f711a18431702ffb9272b0b651985670051
juanMory <spacejamm@gmail.com>
2015-07-14 21:13:32 -0500

    ETNA SendMail

---
+++ ETNASendMail/ETNASendMail/Conexion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConsoleApplicationETNASendMail
+{
+    class Conexion
+    {
+        private static string correo;
+        private static string nomEncuesta;
+        public static void getConexion()
+        {
+            //string server = "JUANCHO-PC\SQLEXPRESS";
+            //Traer PLANTILLA
+            try
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost;Initial Catalog=INTEGRADO;User Id=sa;Password=J@MM1409;Integrated Security=True");
+                cn.Open();
+                string plantilla = "SELECT Descripcion FROM TB_PV_Plantillas WHERE PlantillaID=1;";
+
+                SqlCommand cmdplantilla = new SqlCommand(plantilla, cn);
+                SqlDataReader readerplantilla = cmdplantilla.ExecuteReader();
+                
+                try
+                {
+                    while (readerplantilla.Read())
+                    {
+                        nomEncuesta = (String.Format(" {0}",
+                            readerplantilla[0]));
+                        Console.WriteLine(nomEncuesta);
+                    }
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //Traer Correos
+            try
+            {
+                SqlConnection cn = new SqlConnection("Data Source=localhost;Initial Catalog=INTEGRADO;User Id=sa;Password=J@MM1409;Integrated Security=True");
+                cn.Open();
+                string email = "SELECT Email FROM TB_VT_Clientes WHERE ClienteId in(1,2,3);";
+                SqlCommand cmdemail = new SqlCommand(email, cn);
+                SqlDataReader readeremail = cmdemail.ExecuteReader();
+
+                try
+                {
+                    while (readeremail.Read())
+                    {
+                        correo = (String.Format(" {0}",
+                        readeremail[0]));
+                        Console.WriteLine(correo);
+                    }
+                    SendMail.ETNASendMail(nomEncuesta,correo);
+
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ETNASendMail/ETNASendMail/SendMail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace ConsoleApplicationETNASendMail
+{
+    class SendMail
+    {
+        public static Boolean ETNASendMail(string plantilla, string cuentaDeCorreo)
+        {
+            try
+            {
+                //Configuración del Mensaje
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                //Especificamos el correo desde el que se enviará el Email y el nombre de la persona que lo envía
+                mail.From = new MailAddress("spacejamm@gmail.com", "Juan Mory", Encoding.UTF8);
+                //Aquí ponemos el asunto del correo
+                mail.Subject = "Encuestas ETNA POSTVENTA";
+                //Aquí ponemos el mensaje que incluirá el correo
+                mail.Body = "Encuesta ETNA"+"  :  "+plantilla;
+                //Especificamos a quien enviaremos el Email, no es necesario que sea Gmail, puede ser cualquier otro proveedor
+                mail.To.Add(cuentaDeCorreo);
+                ////Si queremos enviar archivos adjuntos tenemos que especificar la ruta en donde se encuentran
+                //mail.Attachments.Add(new Attachment(@"C:\Documentos\carta.docx"));
+
+                //Configuracion del SMTP
+                SmtpServer.Port = 587; //Puerto que utiliza Gmail para sus servicios
+                //Especificamos las credenciales con las que enviaremos el mail
+                SmtpServer.Credentials = new System.Net.NetworkCredential("spacejamm@gmail.com", "J@MM1409");
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ex.GetBaseException();
+                return false;
+            }
+        }
+    }
+}
